1ee34a341bad01b710486bd59240d1a926d3c951
Kayla Firestack <kfirestack@mbta.com>
2024-02-15 22:12:58 -0500

    init(flake): add nix-beam-flakes template
    ```
    nix flake init -t github:shanesveller/nix-beam-flakes#phoenix
    ```

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Skate: Gliding on Wheels";
+
+  inputs = {
+    beam-flakes = {
+      url = "github:shanesveller/nix-beam-flakes";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs @ {
+    beam-flakes,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [beam-flakes.flakeModule];
+
+      systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
+
+      perSystem = { pkgs, ... }: {
+        beamWorkspace = {
+          enable = true;
+          devShell = {
+            languageServers.elixir = true;
+            languageServers.erlang = false;
+            phoenix = true;
+          };
+          versions = {
+            fromToolVersions = ./.tool-versions;
+          };
+        };
+      };
+    };
+}
