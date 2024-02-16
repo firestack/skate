0e48fb1d4e1474b436767afb37d4de39383b6741
Kayla Firestack <kfirestack@mbta.com>
2024-02-15 22:13:45 -0500

    cleanup(flake): indent with tabs

--- flake.nix
+++ flake.nix
@@ -1,38 +1,38 @@
 {
-  description = "Skate: Gliding on Wheels";
+	description = "Skate: Gliding on Wheels";
 
-  inputs = {
-    beam-flakes = {
-      url = "github:shanesveller/nix-beam-flakes";
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  };
+	inputs = {
+		beam-flakes = {
+			url = "github:shanesveller/nix-beam-flakes";
+			inputs.flake-parts.follows = "flake-parts";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		flake-parts.url = "github:hercules-ci/flake-parts";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+	};
 
-  outputs = inputs @ {
-    beam-flakes,
-    flake-parts,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [beam-flakes.flakeModule];
+	outputs = inputs @ {
+		beam-flakes,
+		flake-parts,
+		...
+	}:
+		flake-parts.lib.mkFlake {inherit inputs;} {
+			imports = [beam-flakes.flakeModule];
 
-      systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
+			systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
 
-      perSystem = { pkgs, ... }: {
-        beamWorkspace = {
-          enable = true;
-          devShell = {
-            languageServers.elixir = true;
-            languageServers.erlang = false;
-            phoenix = true;
-          };
-          versions = {
-            fromToolVersions = ./.tool-versions;
-          };
-        };
-      };
-    };
+			perSystem = { pkgs, ... }: {
+				beamWorkspace = {
+					enable = true;
+					devShell = {
+						languageServers.elixir = true;
+						languageServers.erlang = false;
+						phoenix = true;
+					};
+					versions = {
+						fromToolVersions = ./.tool-versions;
+					};
+				};
+			};
+		};
 }
